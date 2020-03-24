0748d24684fcdaade5e7db5f5a64c9c7bbc57bb0
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2020-03-24 12:12:22 +0530

    Project setup.

---
+++ config/dev.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const sourceDirectory = path.resolve(__dirname, '../src')
+const publicDirectory = path.resolve(__dirname, '../public')
+
+const config = {
+  mode: 'development',
+  entry: sourceDirectory + '/app.js',
+  output: {
+    path: publicDirectory,
+    filename: 'bundle-[name].[hash:8].js',
+    publicPath: ''
+  },
+  resolve: { extensions: ['.js', '.json', '.scss'] },
+  devServer: {
+    // inline: true,
+    port: 31291,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: false,
+      title: 'The Todo App!',
+      template: sourceDirectory + '/index.html',
+      filename: 'index.html' // relative to root of the application
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css-[name].[hash:8].css',
+      chunkFilename: 'csschunk-[name].bundle.css'
+    })
+  ]
+}
+
+module.exports = config
---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Uno!</h2>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('app'))
