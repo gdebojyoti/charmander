f7659d9c171e249d48d89f98276e039a7378e522
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2020-04-05 05:46:26 +0530

    Bugfix: Basic page content overflow issue.

--- src/components/Ui/BasicPage/index.js
+++ src/components/Ui/BasicPage/index.js
@@ -5,14 +5,16 @@ import './style'
 const BasicPage = ({ className = '', heading, subheading, children }) => {
   return (
     <div className={`basic-page ${className}`}>
-      {(heading || subheading) && (
-        <div className='basic-page__headings'>
-          {heading && <h1 className='basic-page__heading'>{heading}</h1>}
-          {subheading && <h2 className='basic-page__subheading'>{subheading}</h2>}
-        </div>
-      )}
+      <div className='basic-page__content'>
+        {(heading || subheading) && (
+          <div className='basic-page__headings'>
+            {heading && <h1 className='basic-page__heading'>{heading}</h1>}
+            {subheading && <h2 className='basic-page__subheading'>{subheading}</h2>}
+          </div>
+        )}
 
-      {children}
+        {children}
+      </div>
     </div>
   )
 }
