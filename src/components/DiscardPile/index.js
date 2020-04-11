644b7ef9fbd60159d7b21b9a7b341ca7b7509b92
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2020-04-12 00:48:32 +0530

    Direction indicator with color.

--- src/components/DiscardPile/index.js
+++ src/components/DiscardPile/index.js
@@ -14,10 +14,19 @@ const DiscardPile = ({ data, isReversed }) => {
   const imageClass = `discard-pile__direction-container ${isReversed ? 'discard-pile__direction-container--reversed' : ''}`
   const discardedCards = data.length > 5 ? data.slice(data.length - 5) : data
 
+  const lastCard = data[data.length - 1]
+  const { color: directionColor } = lastCard || {}
+
+  const directionClass = ['discard-pile__direction']
+  directionColor === 'RED' && directionClass.push('discard-pile__direction--red')
+  directionColor === 'BLUE' && directionClass.push('discard-pile__direction--blue')
+  directionColor === 'YELLOW' && directionClass.push('discard-pile__direction--yellow')
+  directionColor === 'GREEN' && directionClass.push('discard-pile__direction--green')
+
   return (
     <div className='discard-pile'>
       <div className={imageClass}>
-        <div className='discard-pile__direction' />
+        <div className={directionClass.join(' ')} />
       </div>
       <div className='discard-pile__card-container'>
         {discardedCards.map((card, index) => {
