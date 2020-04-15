a95bbd1ac80a7f979f66344f7b1deb5e3303b380
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2020-04-16 04:46:18 +0530

    Player card image border removed.

--- src/components/PlayerCard/index.js
+++ src/components/PlayerCard/index.js
@@ -9,13 +9,13 @@ const PlayerCard = ({ data, isTurn, disabled = false, hideCardCount = false }) =
     img = 'https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png',
     cards = [],
     cardCount = cards.length,
-    borderId
+    borderId // 1 for debojyoti
   } = data
 
   // TODO: find a way to depict online status
 
   const mainClass = `opponent-card ${isTurn ? 'opponent-card--active' : ''} ${disabled ? 'opponent-card--disabled' : ''}`
-  const frameClass = `opponent-card__frameset ${borderId === 1 ? 'opponent-card__frameset--flashy' : ''}`
+  const frameClass = `opponent-card__frameset ${borderId === 100 ? 'opponent-card__frameset--flashy' : ''}`
   const cardCountClass = `opponent-card__card-count ${cardCount < 2 ? 'opponent-card__card-count--warn' : ''}`
 
   return (
